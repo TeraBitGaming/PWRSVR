a6a164866e0a9a2e49faf070d25aec2b3abc8b47
TeraBitGaming <loermansw@gmail.com>
2018-12-20 14:00:57 +0100

    V0.1
    [CURRENT]
    Added snapping
    Added hooks for detecting whether a plug is snapped in/not
    Cleaned up folders
    
    [NEXT]
    Make general game-controller.
    Make game-controller recognise all 3 plugs being plugged in
    
    [NOTES]
    Try to find a more graceful way of making the plugs snap into place

---
+++ Assets/Scripts/SnappingScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnappingScript : MonoBehaviour
+{
+    public Transform Self;
+    public Rigidbody RB;
+    public Transform Target;
+    public float requiredDistance;
+
+    public bool isLocked = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Self.position, Target.position) < requiredDistance){
+            RB.isKinematic = true;
+            gameObject.transform.position = Target.position;
+            gameObject.transform.rotation = Target.rotation;
+            isLocked = true;
+        } else if (Vector3.Distance(Self.position, Target.position) > requiredDistance){
+            RB.isKinematic = false;
+            isLocked = false;
+        }
+    }
+}
