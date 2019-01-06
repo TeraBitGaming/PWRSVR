6b386fb4e9315cb7aa79fefab4c2edf58c84b8c7
TeraBitGaming <loermansw@gmail.com>
2019-01-06 18:44:46 +0100

    V1.3
    [CURRENT]
    Added a general game controller
    Made the console spit out "yippee" whenever all 3 plugs are inserted.
    
    [NEXT]
    Add lights
    Better room hitboxes
    
    [NOTES]
    Snapping script should work for statically rotating objects, but only if I use an empty gameobject + spring joint. Again, "should".

---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+    public int PlugCount = 0;
+
+    public GameObject Plug1;
+    public GameObject Plug2;
+    public GameObject Plug3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlugCount = Plug1.GetComponent<SnappingScript>().IsPlugged + Plug2.GetComponent<SnappingScript>().IsPlugged + Plug3.GetComponent<SnappingScript>().IsPlugged;
+        if (PlugCount == 3){
+            Debug.Log("yippiee!");
+        }
+    }
+}
+// PlugCount = GameController.GetComponent<GameControllerScript>().PlugCount;
--- Assets/Scripts/SnappingScript.cs
+++ Assets/Scripts/SnappingScript.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class SnappingScript : MonoBehaviour
 {
-    public Transform Self;
-    public Rigidbody RB;
+    private Transform Self;
+    private Rigidbody RB;
+    private GameObject GameController;
+    public int IsPlugged = 0;
     public Transform Target;
     public float requiredDistance;
+    
+    private bool isLocked = false;
 
-    public bool isLocked = false;
+    public bool PushPlug = false;
+
+    void Start(){
+        GameController = GameObject.Find("GameStateController");
+        Self = gameObject.GetComponent<Transform>();
+        RB = gameObject.GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,6 +29,9 @@ public class SnappingScript : MonoBehaviour
             gameObject.transform.position = Target.position;
             gameObject.transform.rotation = Target.rotation;
             isLocked = true;
+            if (PushPlug == true){
+                IsPlugged = 1;
+            }
         } else if (Vector3.Distance(Self.position, Target.position) > requiredDistance){
             RB.isKinematic = false;
             isLocked = false;
