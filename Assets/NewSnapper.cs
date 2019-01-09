03628bdcc9896014ead09262fb67b77dfe07769b
TeraBitGaming <loermansw@gmail.com>
2019-01-09 21:45:54 +0100

    V1.1
    [CURRENT]
    Started adding lights, must test mood
    
    [FUTURE]
    Work on turning lights on/off based on plugs, and potentially a light-switch.

---
+++ Assets/NewSnapper.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewSnapper : MonoBehaviour
+{
+    public GameObject target;
+    private Transform targetTransform;
+    private Rigidbody targetRB;
+    public float requiredDistance;
+    public string snapTag;
+    private Transform self;
+    private Rigidbody selfRB;
+    private bool connected;
+
+    public bool lockXRotation;
+    public bool lockYRotation;
+    public bool lockZRotation;
+    public bool lockAllRotation;
+    public bool lockXAxis;
+    public bool lockYAxis;
+    public bool lockZAxis;
+    public bool lockAllAxis;
+    public bool lockAll;
+    public bool copyTransform;
+
+    // Start is called before the first frame update
+    void Start(){
+        self = gameObject.GetComponent<Transform>();
+        selfRB = gameObject.GetComponent<Rigidbody>();
+        targetTransform = target.GetComponent<Transform>();
+        targetRB = target.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Vector3.Distance(self.position, targetTransform.position) < requiredDistance && connected == false){
+            if(copyTransform == true){
+                gameObject.transform.position = targetTransform.position;
+                gameObject.transform.rotation = targetTransform.rotation;
+            }
+            gameObject.AddComponent<SpringJoint>();
+            gameObject.GetComponent<SpringJoint>().connectedBody = targetRB;
+            gameObject.GetComponent<SpringJoint>().spring = 1000;
+            gameObject.GetComponent<SpringJoint>().tolerance = 0;
+            connected = true;
+            if (lockXRotation == true){
+                selfRB.constraints = RigidbodyConstraints.FreezeRotationX;
+            }
+            if (lockYRotation == true){
+                selfRB.constraints = RigidbodyConstraints.FreezeRotationY;
+            }
+            if (lockZRotation == true){
+                selfRB.constraints = RigidbodyConstraints.FreezeRotationZ;
+            }
+            if (lockAllRotation == true){
+                selfRB.constraints = RigidbodyConstraints.FreezeRotation;
+            }
+            if (lockXAxis == true){
+                selfRB.constraints = RigidbodyConstraints.FreezePositionX;
+            }
+            if (lockYAxis == true){
+                selfRB.constraints = RigidbodyConstraints.FreezePositionY;
+            }
+            if (lockZAxis == true){
+                selfRB.constraints = RigidbodyConstraints.FreezePositionZ;
+            }
+            if (lockAllAxis == true){
+                selfRB.constraints = RigidbodyConstraints.FreezePosition;
+            }
+            if (lockAll == true){
+                selfRB.constraints = RigidbodyConstraints.FreezeAll;
+            }
+            
+        } else if(Vector3.Distance(self.position, targetTransform.position) > requiredDistance){
+            Destroy(gameObject.GetComponent<SpringJoint>());
+            selfRB.constraints = RigidbodyConstraints.None;
+            connected = false;
+        }
+    }
+}
